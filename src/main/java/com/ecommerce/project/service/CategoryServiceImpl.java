e9aebfd7bd99e681e6251a8e7d6d7c3a55c128e5
Aaryamanjhatakia <149300617+Aaryamanjhatakia@users.noreply.github.com>
2026-01-30 12:09:05 +0530

    Implemented auto-assigning of id, Delete Category and ResponseEntity
    understood and implemented:
    1) the auto-assignment of ids.
    2) Delete Category
    3) Handling status codes....using ResponseEntity class and learned how to pass response entity instead of string in the DELETE method.

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,7 +1,11 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +14,7 @@ import java.util.List;
 @Service
 public class CategoryServiceImpl implements CategoryService{
     private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories() {
@@ -18,6 +23,20 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category);
     }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                .filter(c -> c.getCategoryId().equals(categoryId))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Resource not found"));
+
+
+        categories.remove(category);
+        return "Category with categoryId: " + categoryId + " deleted successfully!";
+    }
+
 }
